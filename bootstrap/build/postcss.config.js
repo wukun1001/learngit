e112497b485e6e71bc8ab988cbd5d10ae863926f
wukun1001 <17770033768@163.com>
2017-09-27 19:33:32 +0800

    bootstrap

---
+++ bootstrap/build/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = (ctx) => ({
+  map: ctx.file.dirname.startsWith('docs') ? false : {
+    inline: false,
+    annotation: true,
+    sourcesContent: true
+  },
+  plugins: {
+    autoprefixer: {}
+  }
+})
